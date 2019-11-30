bbee83d9096f65bbbce6ede952087293d72b3b09
Paul Vazquez <pev@live.com.mx>
2019-11-29 18:28:07 -0600

    Added M and S

--- main.py
+++ main.py
@@ -4,12 +4,26 @@ from FIFO import FIFO
 from Page import Page 
 from Process import Process 
 
+#Global Variables
+
 M = np.zeros(128)
 S = np.zeros(256)
+lru = LRU()
+fifo = FIFO()
+
+# Ask for N-bytes in memory
+def P(bytes,process_id):
+    print("Asks for memory")
+
+
 
 #Returns the number of non-zero elements in memory (# of pages)
 def memory_available(memory):
     return len(memory) - np.count_nonzero(memory)
+
+
+
+##Read from file 
 
 
 print(memory_available(M))
